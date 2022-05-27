812ea1ce942e7274847da21385174aec2f34aee6
Jose Maria <jm1a192@ieszaidinvergeles.org>
2022-05-27 09:04:25 +0200

    añadiendo los metodos if y else a la clase

--- Practica1/src/main/java/Practica1/Metodos.java
+++ Practica1/src/main/java/Practica1/Metodos.java
@@ -6,6 +6,22 @@ package Practica1;
  * @author SI
  */
 public class Metodos {
+    public String clasificacion(String tipo){
+        String clasificacion = "";
+        
+        if(tipo.equals("niños")){
+            clasificacion = "E Todo el publico";
+        }else if(tipo.equals("Jovenes")){
+            clasificacion = "T adolescentes";
+        }else if(tipo.equals("Mayores de edad")){
+            clasificacion = "M mayores";
+        }else if(tipo.equals("Adultos")){
+            clasificacion = "A adultos";
+        }else{
+            clasificacion = "RP Pendientes";
+    }
+        
+        return clasificacion;
     
     }
     
