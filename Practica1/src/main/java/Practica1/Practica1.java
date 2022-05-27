ae39899d5f9aa420b00b0460ec51cb191e8aada8
Jose Maria <jm1a192@ieszaidinvergeles.org>
2022-05-27 08:59:23 +0200

    añadiendo codigo nuevo

---
+++ Practica1/src/main/java/Practica1/Practica1.java
@@ -0,0 +1,30 @@
+
+package Practica1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author joxex
+ */
+public class Practica1 {
+
+    
+    public static void main(String[] args) {
+        Juegos_de_video();
+    }
+    public static void Juegos_de_video(){
+        Scanner in = new Scanner(System.in);
+        String nombre,publico,clasificacion;
+        double precio,total;
+        System.out.println("Clasificador de Juegos de Video");
+        System.out.println("Ingrese el numero del juego");
+        nombre = in.next();
+        System.out.println("Ingrese quienes pueden jugarlo: niños,jovenes,mayores de edad, adultos o en blanco si no sabe");
+        publico = in.next();
+        System.out.println("Ingrese el precio del juego");
+        precio = in.nextDouble();
+        Metodos met = new Metodos();
+        
+    }
+}
